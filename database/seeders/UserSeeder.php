f4f3a9c6860d22391d3377ae136f2a7a5df6b8e6
tri sela <triselawati55@gmail.com>
2024-09-12 09:08:47 +0700

    menyusun layout

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illmunite\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
